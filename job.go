5a861f02944132ab456dcc171b132e3b360e3847
kireledan <erik.nadel@rallyhealth.com>
2021-01-15 12:26:16 -0600

    Add Choices to parameter definition

--- job.go
+++ job.go
@@ -52,6 +52,7 @@ type ParameterDefinition struct {
 	Description string `json:"description"`
 	Name        string `json:"name"`
 	Type        string `json:"type"`
+	Choices     []string `json:"choices,omitempty"`
 }
 
 type JobResponse struct {
